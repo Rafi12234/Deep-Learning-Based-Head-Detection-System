4b1f7dfd8270c671fafaa541b4969e5c6ed51934
Rafi12234 <shajidulkabir12345@gmail.com>
2026-04-25 04:16:45 +0600

    chore: add frontend/src/components/Layout.jsx (43/64)

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-100">
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <div className="flex flex-1 flex-col">
+          <Navbar />
+          <main className="flex-1 p-4 md:p-6">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
