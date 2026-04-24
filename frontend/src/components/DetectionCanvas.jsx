41fa5f4911c6931299ba3a786d61533ed7ca48be
Rafi12234 <shajidulkabir12345@gmail.com>
2026-04-25 04:16:45 +0600

    chore: add frontend/src/components/DetectionCanvas.jsx (39/64)

---
+++ frontend/src/components/DetectionCanvas.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from 'react';
+import { drawBoxes } from '../utils/drawBoxes';
+
+export default function DetectionCanvas({ detections, frameSize }) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    drawBoxes(canvasRef.current, detections, frameSize);
+  }, [detections, frameSize]);
+
+  return <canvas ref={canvasRef} className="pointer-events-none absolute inset-0 h-full w-full" />;
+}
